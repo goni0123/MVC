20f3723844beebb68d18937c5001394dec2890ce
Agon Muharemi <Goni_M2013@hotmail.com>
2023-10-08 11:05:10 +0200

    Implementing CRUD operations for Category

--- MVCPro/Controllers/CategoryController.cs
+++ MVCPro/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCPro.Data;
 using MVCPro.Models;
+using System.Text.RegularExpressions;
 
 namespace MVCPro.Controllers
 {
@@ -22,14 +23,69 @@ namespace MVCPro.Controllers
         }
         [HttpPost]
         public IActionResult Create(Category obj)
-        {   
-            if (!ModelState.IsValid)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
             {
                 _db.categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.categories.Find(id);
+            //Category? categoryFromDb1 = _db. Categories. FirstOrDefault (u=>u. Id==id);
+            //Category? categoryFromDb2 = _db. Categories. Where (u=>u. Id==id). FirstOrDefault();k
+            if (categoryFromDb == null)
+            {
+                return NotFound(id);
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound(id);
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category obj =_db.categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
