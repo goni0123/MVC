6f406f92986f5522e086294cc34bb77cd7a8bb17
Agon Muharemi <Goni_M2013@hotmail.com>
2023-10-11 19:51:59 +0200

    Razor

--- WebRazor_Temp/Program.cs
+++ WebRazor_Temp/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebRazor_Temp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
