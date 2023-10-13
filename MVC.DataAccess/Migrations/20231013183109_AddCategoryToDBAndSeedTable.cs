3115ebdaf7a02e1c2b59c8cf7134ddc87ef7fcc1
Agon Muharemi <Goni_M2013@hotmail.com>
2023-10-13 20:36:36 +0200

    Completed

--- MVC.DataAccess/Migrations/20231013183109_AddCategoryToDBAndSeedTable.cs
+++ MVC.DataAccess/Migrations/20231013183109_AddCategoryToDBAndSeedTable.cs
@@ -2,10 +2,12 @@
 
 #nullable disable
 
-namespace MVCPro.DataAccess.Migrations
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MVC.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class AddCategory : Migration
+    public partial class AddCategoryToDBAndSeedTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -16,12 +18,22 @@ namespace MVCPro.DataAccess.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
                     DisplayOrder = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "Test" },
+                    { 3, 5, "Aon" }
                 });
         }
 
