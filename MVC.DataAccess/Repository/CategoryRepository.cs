3c6461d6275200d95e6acd45c8913c03355e3be6
Agon Muharemi <Goni_M2013@hotmail.com>
2023-10-13 22:48:43 +0200

    Needs Fixing of the Repository Pattern

---
+++ MVC.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using MVC.Models;
+using MVCPro.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MVC.DataAccess.Repository.IRepository;
+
+namespace MVC.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly AppDBContext _db;
+
+        public CategoryRepository(AppDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.categories.Update(obj);
+        }
+    }
+}
--- MVCPro/Controllers/CategoryController.cs
+++ MVCPro/Controllers/CategoryController.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC.Models;
-using MVCPro.DataAccess.Data;
+using MVC.DataAccess.Repository;
 using System.Text.RegularExpressions;
+using MVC.DataAccess.Repository.IRepository;
 
 namespace MVCPro.DataAccess.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly AppDBContext _db;
-        public CategoryController(AppDBContext db) 
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository categoryRepo) 
         { 
-            _db = db;
+            _categoryRepo=categoryRepo;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.categories.ToList(); 
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList(); 
             return View(objCategoryList);
         }
         public IActionResult Create()
@@ -30,8 +31,8 @@ namespace MVCPro.DataAccess.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.categories.Add(obj);
-                _db.SaveChanges();
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
@@ -43,7 +44,7 @@ namespace MVCPro.DataAccess.Controllers
             {
                 return NotFound();
             }
-            Category categoryFromDb = _db.categories.Find(id);
+            Category categoryFromDb = _categoryRepo.Get(u => u.Id == id);
             //Category? categoryFromDb1 = _db. Categories. FirstOrDefault (u=>u. Id==id);
             //Category? categoryFromDb2 = _db. Categories. Where (u=>u. Id==id). FirstOrDefault();k
             if (categoryFromDb == null)
@@ -57,8 +58,8 @@ namespace MVCPro.DataAccess.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
@@ -70,7 +71,7 @@ namespace MVCPro.DataAccess.Controllers
             {
                 return NotFound();
             }
-            Category categoryFromDb = _db.categories.Find(id);
+            Category categoryFromDb = _categoryRepo.Get(u => u.Id == id);
             if (categoryFromDb == null)
             {
                 return NotFound(id);
@@ -80,13 +81,13 @@ namespace MVCPro.DataAccess.Controllers
         [HttpPost,ActionName("Delete")]
         public IActionResult DeletePost(int? id)
         {
-            Category obj =_db.categories.Find(id);
-            if(obj == null)
+            Category obj = _categoryRepo.Get(u => u.Id == id);
+            if (obj == null)
             {
                 return NotFound();
             }
-            _db.categories.Remove(obj);
-            _db.SaveChanges(); 
+            _categoryRepo.Remove(obj);
+            _categoryRepo.Save(); 
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
