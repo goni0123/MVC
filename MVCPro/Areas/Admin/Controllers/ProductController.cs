f5346b2e7a4ffd7da8f571f6d77c54b3eefa75e6
Agon Muharemi <Goni_M2013@hotmail.com>
2023-10-17 16:07:40 +0200

    Image getting and storing impemented

--- MVCPro/Areas/Admin/Controllers/ProductController.cs
+++ MVCPro/Areas/Admin/Controllers/ProductController.cs
@@ -10,9 +10,11 @@ namespace MVCPro.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unit;
-        public ProductController(IUnitOfWork unit)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unit, IWebHostEnvironment webHostEnvironment)
         {
             _unit = unit;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -20,9 +22,9 @@ namespace MVCPro.Areas.Admin.Controllers
             return View(objProductList);
 
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            ProductVM productVM = new ()
+            ProductVM productVM = new()
             {
                 CategoryList = _unit.Category
                 .GetAll().Select(u => new SelectListItem
@@ -32,13 +34,59 @@ namespace MVCPro.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                //create
+                return View(productVM);
+            }
+            else
+            {
+                //update
+                productVM.Product = _unit.Product.Get(u => u.Id == id);
+                return View(productVM); // Add this line
+            }
         }
+
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
+
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var oldImagePath =
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unit.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unit.Product.Update(productVM.Product);
+                }
                 _unit.Product.Add(productVM.Product);
                 _unit.Save();
                 TempData["success"] = "Product created successfully";
@@ -52,37 +100,10 @@ namespace MVCPro.Areas.Admin.Controllers
                     Text = u.Name,
                     Value = u.Id.ToString()
                 });
-
                 return View(productVM);
             }
         }
-        public IActionResult Edit(int id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product ProductFromDb = _unit.Product.Get(u => u.Id == id);
-            //Product? ProductFromDb1 = _db. Categories. FirstOrDefault (u=>u. Id==id);
-            //Product? ProductFromDb2 = _db. Categories. Where (u=>u. Id==id). FirstOrDefault();k
-            if (ProductFromDb == null)
-            {
-                return NotFound(id);
-            }
-            return View(ProductFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unit.Product.Update(obj);
-                _unit.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+
         public IActionResult Delete(int id)
         {
             if (id == null || id == 0)
